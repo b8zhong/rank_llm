0cbe96cfab85fc0b226a0ab9813c517bf6303307
Ronak <ronakice@hotmail.com>
2024-10-02 20:17:26 -0400

    update

--- src/rank_llm/rerank/rank_listwise_os_llm.py
+++ src/rank_llm/rerank/rank_listwise_os_llm.py
@@ -94,7 +94,7 @@ class RankListwiseOSLLM(RankLLM):
                 download_dir=os.getenv("HF_HOME"),
                 enforce_eager=False,
                 tensor_parallel_size=num_gpus,
-                max_model_len=111232,
+                max_model_len=108064,
                 ignore_patterns=ignore_patterns,
             )
             self._tokenizer = self._llm.get_tokenizer()
